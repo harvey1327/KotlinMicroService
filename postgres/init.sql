5d29e01b87d9e5093390927477b77743a7321c54
Harvey Bal <harvey.bal@paconsulting.com>
2018-05-04 15:56:32 +0100

    Updated init.sql for accounts

--- postgres/init.sql
+++ postgres/init.sql
@@ -1,6 +1,5 @@
 GRANT ALL PRIVILEGES ON DATABASE sparkservice TO sparkuser;
-CREATE TABLE usertable(
-user_id serial PRIMARY KEY,
-user_name VARCHAR (50) NOT NULL,
-user_email VARCHAR (100) UNIQUE NOT NULL
+CREATE TABLE accounts(
+"id" serial PRIMARY KEY,
+"name" VARCHAR (50) NOT NULL
 );
