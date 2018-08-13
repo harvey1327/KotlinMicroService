cdbdc710d758c232361fea24d057909aacf682ed
Harvey Bal <harvey.bal@paconsulting.com>
2018-08-13 16:53:48 +0100

    Moved Account table into domain

--- src/main/kotlin/org/hrv/web/domain/Account.kt
+++ src/main/kotlin/org/hrv/web/domain/Account.kt
@@ -1,3 +1,10 @@
 package org.hrv.web.domain
 
+import org.jetbrains.exposed.sql.Table
+
 data class Account(val name: String, val id: Int)
+
+object AccountTable : Table() {
+    val id = integer("id").primaryKey().autoIncrement()
+    val name = varchar("name", 50)
+}
