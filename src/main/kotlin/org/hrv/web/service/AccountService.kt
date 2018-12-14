769952667e148e292b1de5778ae1343cf80c0154
Harvey Bal <harvey.bal@paconsulting.com>
2018-12-14 11:30:00 +0000

    Made db injected as a dependency

--- src/main/kotlin/org/hrv/web/service/AccountService.kt
+++ src/main/kotlin/org/hrv/web/service/AccountService.kt
@@ -4,15 +4,15 @@ import org.hrv.web.domain.ReadAccount
 import org.hrv.web.domain.WriteAccount
 import org.hrv.web.service.database.DataBaseService
 
-class AccountService {
+class AccountService(private val dataBaseService: DataBaseService) {
 
-    suspend fun getAllAccounts(): List<ReadAccount> = DataBaseService().getAllAccounts()
+    suspend fun getAllAccounts(): List<ReadAccount> = dataBaseService.getAllAccounts()
 
-    suspend fun getAccountById(id: Int): ReadAccount? = DataBaseService().getAccountById(id)
+    suspend fun getAccountById(id: Int): ReadAccount? = dataBaseService.getAccountById(id)
 
-    suspend fun createAccount(account: WriteAccount): ReadAccount = DataBaseService().createAccount(account)
+    suspend fun createAccount(account: WriteAccount): ReadAccount = dataBaseService.createAccount(account)
 
-    suspend fun updateAccount(account: WriteAccount, id: Int): ReadAccount? = DataBaseService().updateAccount(account, id)
+    suspend fun updateAccount(account: WriteAccount, id: Int): ReadAccount? = dataBaseService.updateAccount(account, id)
 
-    suspend fun deleteAccount(id: Int): Boolean = DataBaseService().deleteAccount(id)
+    suspend fun deleteAccount(id: Int): Boolean = dataBaseService.deleteAccount(id)
 }
